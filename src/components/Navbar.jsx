6cf4dd28e418851621d947c66cf4b1ead664a03b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-18 07:43:36 +0000

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import { appleImg, bagImg, searchImg } from "../assets";
+import { navLists } from "../constants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="flex w-full items-center justify-between p-5 sm:px-10">
+      <nav className="screen-max-width flex w-full">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="text-gray cursor-pointer px-5 text-sm transition-all hover:text-white"
+            >
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
+          <img src={searchImg} alt="Search" width={18} height={18} />
+          <img src={bagImg} alt="Bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
