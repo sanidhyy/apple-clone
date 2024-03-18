887373081540f0f8842a147680b022b28b6d619d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-18 07:13:45 +0000

    color variants added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+      },
+    },
   },
   plugins: [],
 };
