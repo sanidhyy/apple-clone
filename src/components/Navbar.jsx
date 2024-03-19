56156c4ff627667caeded31813301d297774a154
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-19 11:56:08 +0000

    navbar links fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
   return (
     <header className="flex w-full items-center justify-between p-5 sm:px-10">
       <nav className="screen-max-width flex w-full">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <a href="#">
+          <img src={appleImg} alt="Apple" width={14} height={18} />
+        </a>
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
@@ -21,8 +23,12 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
-          <img src={searchImg} alt="Search" width={18} height={18} />
-          <img src={bagImg} alt="Bag" width={18} height={18} />
+          <a href="#">
+            <img src={searchImg} alt="Search" width={18} height={18} />
+          </a>
+          <a href="#">
+            <img src={bagImg} alt="Bag" width={18} height={18} />
+          </a>
           <a href={sourceCode} target="_blank" rel="noreferrer noopener">
             <img src={githubImg} alt="Bag" width={18} height={18} />
           </a>
