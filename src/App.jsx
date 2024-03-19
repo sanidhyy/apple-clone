4d50972d9fb0ca3474554aea07f9e7e62d68bed6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-19 06:06:48 +0000

    highlights created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import { rightImg, watchImg } from "../assets";
+
 const Highlights = () => {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []);
+
+  return (
+    <section
+      id="highlights"
+      className="common-padding bg-zinc h-full w-screen overflow-hidden"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="Watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="Right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
