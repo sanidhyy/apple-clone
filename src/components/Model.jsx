995d1eea1ab7e35e8462c2558d426931e7a783cf
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-19 07:57:09 +0000

    iphone model section completed

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,14 +1,15 @@
-import React, { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { useGSAP } from "@gsap/react";
-import { View } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+
 import gsap from "gsap";
 import * as THREE from "three";
 
-import ModelView from "./ModelView";
-import { yellowImg } from "../assets";
 import { models, sizes } from "../constants";
+import ModelView from "./ModelView";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -30,11 +31,26 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
+
   useGSAP(() => {
-    gsap.to("#heading", {
-      y: 0,
-      opacity: 1,
-    });
+    gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
 
   return (
@@ -44,36 +60,36 @@ const Model = () => {
           Take a closer look.
         </h1>
 
-        <div className="mt-5 flex flex-col items-center">
-          <div className="relative h-[75vh] w-full overflow-hidden md:h-[90vh]">
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
             <ModelView
               index={1}
               groupRef={small}
               gsapType="view1"
               controlRef={cameraControlSmall}
-              setRotationScale={setSmallRotation}
+              setRotationState={setSmallRotation}
               item={model}
               size={size}
             />
 
             <ModelView
-              index={1}
+              index={2}
               groupRef={large}
               gsapType="view2"
               controlRef={cameraControlLarge}
-              setRotationScale={setLargeRotation}
+              setRotationState={setLargeRotation}
               item={model}
               size={size}
             />
 
             <Canvas
-              className="size-full"
+              className="w-full h-full"
               style={{
                 position: "fixed",
                 top: 0,
                 bottom: 0,
-                right: 0,
                 left: 0,
+                right: 0,
                 overflow: "hidden",
               }}
               eventSource={document.getElementById("root")}
@@ -83,19 +99,16 @@ const Model = () => {
           </div>
 
           <div className="mx-auto w-full">
-            <p className="mb-5 text-center text-sm font-light">{model.title}</p>
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
 
             <div className="flex-center">
               <ul className="color-container">
                 {models.map((item, i) => (
                   <li
                     key={i}
-                    className="mx-2 size-6 cursor-pointer rounded-full"
-                    style={{
-                      backgroundColor: item.color[0],
-                    }}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                    style={{ backgroundColor: item.color[0] }}
                     onClick={() => setModel(item)}
-                    title="iPhone color selector"
                   />
                 ))}
               </ul>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { Suspense } from "react";
 
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import * as THREE from "three";
+
+import IPhone from "./IPhone";
+import Lights from "./Lights";
+import Loader from "./Loader";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
 };
 
 export default ModelView;
